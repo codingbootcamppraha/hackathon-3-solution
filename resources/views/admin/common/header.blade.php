46182343ad3d6fba1b898cf260e3ade269d8a70d
David <david_ballesteros@data4you.cz>
2023-10-20 17:09:27 +0200

    Updated version

--- resources/views/admin/common/header.blade.php
+++
@@ -1,7 +0,0 @@
-<header class="admin-header">
-    <a class="site-logo" href="{{ route('index') }}">
-        <img src="/img/hector.jpg" alt="St. Hector">
-    </a>
-
-    <div>St. Hector's Administration</div>
-</header>
